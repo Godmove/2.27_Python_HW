4b54a3701833040bd113f0ad78032c42a7140099
Jason Chen <460686278@qq.com>
2018-02-27 23:32:04 +0800

    Update #3.py

--- #3.py
+++ #3.py
@@ -2,5 +2,5 @@ start = input('Enter the start integer:')
 start = int(start)
 end = input('Enter the end integer:')
 end = int(end)
-for i in range(start, end):
+for i in range(start, end + 1):
     print(i, end = ' ')
